f17b0d8d616729f01ab390c61af388b5dd7e335e
Dulmandakh <dulmandakh@gmail.com>
2017-07-07 01:41:05 +0800

    add configuration support

--- index.js
+++ index.js
@@ -8,9 +8,23 @@ var fbTransform = require('fastboot-transform');
 
 module.exports = {
   name: 'ember-cli-owl-carousel2',
+  getConfig() {
+    return Object.assign({
+      css: true,
+      theme: 'default',
+    }, this.app.options['ember-cli-owl-carousel2']);
+  },
   included() {
     this._super(...arguments);
+    const config = this.getConfig();
+
     this.import('vendor/owl.carousel.js');
+    if (config.css) {
+      this.import('vendor/assets/owl.carousel.css');
+      if (config.theme instanceof String) {
+        this.import(`vendor/assets/owl.theme.${config.theme}.css`);
+      }
+    }
   },
   treeForVendor(vendorTree) {
     this._super(...arguments);
