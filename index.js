b4601b1a24d903f0f73b2a205c29af2d64528bca
Dulmandakh <dulmandakh@gmail.com>
2017-07-06 23:12:22 +0800

    add fastboot support

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 /* eslint-env node */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+var fbTransform = require('fastboot-transform');
+
 module.exports = {
-  name: 'ember-cli-owl-carousel'
+  name: 'ember-cli-owl-carousel',
+  included() {
+    this._super(...arguments);
+    this.import('vendor/owl.carousel.js');
+  },
+  treeForVendor(vendorTree) {
+    this._super(...arguments);
+    var trees = [];
+    var modulePath = path.dirname(require.resolve('owl.carousel'));
+
+    if (vendorTree) {
+      trees.push(vendorTree);
+    }
+
+    trees.push(
+      fbTransform(new Funnel(modulePath))
+    );
+    return mergeTrees(trees);
+  }
 };
